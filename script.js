0dbf1b58fe200b5a33000de68ab4487502080e46
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-05 22:58:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+import * as THREE from "three";
 
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x222244);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+scene.add(box);
+camera.position.z = 3;
+
+function animate() {
+  requestAnimationFrame(animate);
+  box.rotation.y += 0.02;
+  renderer.render(scene, camera);
+}
+
+animate();
