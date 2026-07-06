b6027a7056c2aa1c68e8f6b6d02c85360cdd95a0
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-06 15:19:10 +0530

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,8 +1,10 @@
 import * as THREE from "three";
 
+// Scene
 export const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87ceeb);
 
+// Camera
 export const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -11,7 +13,9 @@ export const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.set(0, 6, 10);
+camera.lookAt(0, 0, 0);
 
+// Renderer
 export const renderer = new THREE.WebGLRenderer({
   antialias: true
 });
@@ -19,6 +23,8 @@ export const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+document.body.style.margin = "0";
+document.body.style.overflow = "hidden";
 document.body.appendChild(renderer.domElement);
 
 // Lights
@@ -26,5 +32,19 @@ const ambient = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambient);
 
 const sun = new THREE.DirectionalLight(0xffffff, 2);
-sun.position.set(10, 20, 10);
+sun.position.set(20, 30, 20);
 scene.add(sun);
+
+// Resize
+window.addEventListener("resize", () => {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+  );
+
+});
