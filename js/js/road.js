7564ffedc6de926edbd49802b1bfa61a2a8f3b7a
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-06 14:15:01 +0530

    Update road.js

--- js/js/road.js
+++ js/js/road.js
@@ -1,1 +1,31 @@
 
+import * as THREE from "three";
+
+export const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x87ceeb);
+
+export const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.set(0, 6, 10);
+
+export const renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+document.body.appendChild(renderer.domElement);
+
+// Lights
+const ambient = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambient);
+
+const sun = new THREE.DirectionalLight(0xffffff, 2);
+sun.position.set(10, 20, 10);
+scene.add(sun);
