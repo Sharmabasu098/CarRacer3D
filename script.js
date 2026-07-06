67fd1438dc846a9df6e30d68eab4bf4cd204466e
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-06 14:49:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+import { scene, camera, renderer } from "./js/scene.js";
+import { createRoad, updateRoad } from "./js/road.js";
 
+// Create Game World
+createRoad();
+
+// Animation Loop
+function animate() {
+
+  requestAnimationFrame(animate);
+
+  // Update Road
+  updateRoad();
+
+  // Render Scene
+  renderer.render(scene, camera);
+
+}
+
+animate();
+
+// Resize
+window.addEventListener("resize", () => {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+  );
+
+});
